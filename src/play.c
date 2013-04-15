03f9c983e9b8a53d6e386a6de2a46b1aecbd50f7
Łukasz Komsta <22728459+luqqe@users.noreply.github.com>
2013-04-15 16:34:30 +0000

    Play small fixes

--- src/play.c
+++ src/play.c
@@ -1,4 +1,4 @@
-/*$T src/play.c GC 1.140 04/14/13 16:55:12 */
+/*$T src/play.c GC 1.140 04/15/13 18:32:57 */
 
 
 /*$6
@@ -29,28 +29,32 @@ int						format, stereo;
 int						speed;
 #endif
 
+/*
+ =======================================================================================================================
+ =======================================================================================================================
+ */
 void cwstudio_callback()
 {
-			if(waveOutUnprepareHeader(h, &wh, sizeof(wh)) != MMSYSERR_NOERROR);
-			if(waveOutClose(h) != MMSYSERR_NOERROR);
-			CloseHandle(d);
+	if(waveOutUnprepareHeader(h, &wh, sizeof(wh)) != MMSYSERR_NOERROR);
+	if(waveOutClose(h) != MMSYSERR_NOERROR);
+	CloseHandle(d);
 }
+
 /*
  =======================================================================================================================
  =======================================================================================================================
  */
 int cwstudio_play(cw_sample *sample)
 {
-#ifdef HAVE_LIBWINMM
 	switch(status)
 	{
-	case CWPLAYING:
-		{
+	case CWPLAYING: { 
+#ifdef HAVE_LIBWINMM
 			waveOutReset(h);
+#endif
 		}
-
-	case CWSTOPPED:
-		{
+	case CWSTOPPED: { 
+#ifdef HAVE_LIBWINMM
 			wf.wFormatTag = WAVE_FORMAT_PCM;
 			wf.nChannels = 1;
 			wf.wBitsPerSample = sample->bits;
@@ -67,47 +71,54 @@ int cwstudio_play(cw_sample *sample)
 			if(waveOutPrepareHeader(h, &wh, sizeof(wh)) != MMSYSERR_NOERROR);
 			ResetEvent(d);
 			if(waveOutWrite(h, &wh, sizeof(wh)) != MMSYSERR_NOERROR);
+#elif defined HAVE_PULSEAUDIO
+			if((sample->bits == 8))
+				pas.format = PA_SAMPLE_U8;
+			else
+				pas.format = PA_SAMPLE_S16LE;
+			pas.rate = sample->samplerate;
+			pas.channels = 1;
+			if(!(pa = pa_simple_new(NULL, "qrq", PA_STREAM_PLAYBACK, NULL, "playback", &pas, NULL, NULL, &e))) {
+				fprintf(stderr, "pa_simple_new() failed: %s\n", pa_strerror(e));
+			}
+
+			pa_simple_write(pa, sample->data, (sample->bits / 8) * sample->length - 2, &e);
+			pa_simple_drain(pa, &e);
+#elif defined HAVE_OSS
+			if((audio = open("/dev/dsp", O_WRONLY, 0)) == -1);
+			if((sample->bits == 8))
+				format = AFMT_U8;
+			else
+				format = AFMT_S16_LE;
+			if(ioctl(audio, SNDCTL_DSP_SETFMT, &format) == -1);
+			stereo = 0;
+			if(ioctl(audio, SNDCTL_DSP_STEREO, &stereo) == -1);
+			speed = sample->samplerate;
+			if(ioctl(audio, SNDCTL_DSP_SPEED, &speed) == -1);
+			if(write(audio, sample->data, (sample->bits / 8) * sample->length - 2) == -1);
+			if(close(audio) == -1);
+#endif
 
 			/*
 			 * if(WaitForSingleObject(d, INFINITE) != WAIT_OBJECT_0);
 			 * while (playing);
-			 * 
 			 */
 			status = CWPLAYING;
 			break;
 		}
-
-	case CWPAUSED:
-		{
+	case CWPAUSED:	{ 
+#ifdef HAVE_LIBWINMM
 			waveOutRestart(h);
+#endif
 			status = CWPLAYING;
 		}
 	}
-#endif
+
 	return(status);
 }
 
 /*
  =======================================================================================================================
-    #elif defined HAVE_PULSEAUDIO if((sample->bits == 8)) pas.format = PA_SAMPLE_U8;
-    else pas.format = PA_SAMPLE_S16LE;
-    pas.rate = sample->samplerate;
-    pas.channels = 1;
-    if(!(pa = pa_simple_new(NULL, "qrq", PA_STREAM_PLAYBACK, NULL, "playback", &pas, NULL, NULL, &e))) {
-    fprintf(stderr, "pa_simple_new() failed: %s\n", pa_strerror(e));
-    } pa_simple_write(pa, sample->data, (sample->bits / 8) * sample->length - 2, &e);
-    pa_simple_drain(pa, &e);
-    #elif defined HAVE_OSS if((audio = open("/dev/dsp", O_WRONLY, 0)) == -1);
-    if((sample->bits == 8)) format = AFMT_U8;
-    else format = AFMT_S16_LE;
-    if(ioctl(audio, SNDCTL_DSP_SETFMT, &format) == -1);
-    stereo = 0;
-    if(ioctl(audio, SNDCTL_DSP_STEREO, &stereo) == -1);
-    speed = sample->samplerate;
-    if(ioctl(audio, SNDCTL_DSP_SPEED, &speed) == -1);
-    if(write(audio, sample->data, (sample->bits / 8) * sample->length - 2) == -1);
-    if(close(audio) == -1);
-    #endif }
  =======================================================================================================================
  */
 int cwstudio_pause()
@@ -130,10 +141,8 @@ int cwstudio_pause()
  */
 int cwstudio_stop()
 {
-	if((status == CWPLAYING) || (status == CWSTOPPED)) {
-		waveOutReset(h);
-		status = CWSTOPPED;
-	}
+	waveOutReset(h);
+	status = CWSTOPPED;
 
 	return(status);
 }
