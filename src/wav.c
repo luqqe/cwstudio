6da4adc83f73f704c143c9ad4f653bf3c7f79baf
Łukasz Komsta <22728459+luqqe@users.noreply.github.com>
2016-11-30 18:55:25 +0000

    DOS default samplerate, dates fix, wx-config fix

--- src/wav.c
+++ src/wav.c
@@ -4,7 +4,7 @@
 
     This file is part of CWStudio.
 
-    Copyright 2008-2011 Lukasz Komsta, SP8QED
+    Copyright 2008-2016 Lukasz Komsta, SP8QED
 
     CWStudio is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
