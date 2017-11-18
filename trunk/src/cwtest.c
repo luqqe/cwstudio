3762ddca8c826bf4e5816e21c195f8d8d8c78bf6
luqqe <luqqe@78cbaeba-87df-4710-873f-822178a123d9>
2017-11-18 22:41:04 +0000

    testing code

---
+++ trunk/src/cwtest.c
@@ -0,0 +1,91 @@
+/*$T /cwtest.c GC 1.150 2016-12-26 17:33:58 */
+
+/*$I0 
+
+    This file is part of CWStudio.
+
+    Copyright 2008-2017 Lukasz Komsta, SP8QED
+
+    CWStudio is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CWStudio is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CWStudio. If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include "cwgen.h"
+#include <time.h>
+#include <stdlib.h>
+
+/*
+ =======================================================================================================================
+ This is a testing procedure to catch bugs at non-common CW parameters
+ =======================================================================================================================
+ */
+int main()
+{
+	/*~~~~~~~~~~~~~~~~~~~~~~~*/
+	char		*morsetext,*encoded;
+	cw_sample	asound, csound;
+	cw_param	param;
+
+	/*~~~~~~~~~~~~~~~~~~~~~~~*/
+	cw_initparam(&param);
+	int i;
+
+	srand(time(0));
+
+for (i = 0; i < 100; i++) 
+{
+	
+	printf("\n\n============== TEST %04i ================\n",i);	
+	printf("\n\nagc %05i \n",param.agc = rand() % 2 * rand() % 101);
+	printf("click %05i \n",param.click = rand() % 2 * rand() % 101);
+	printf("cspaces %05i \n",param.cspaces = rand() % 2 * rand() % 101);
+	printf("dashlen %05i \n",param.dashlen = 100 + rand() % 2 * rand() % 901);
+	printf("detune %05i \n",param.detune = rand() % 2 * rand() % 101);
+	printf("even %05i \n",param.even = rand() % 2 * rand() % 101);
+	printf("freq %05i \n",param.freq = 50 + rand() % 3951);
+	printf("hand %05i \n",param.hand = rand() % 2 * rand() % 101);
+	printf("lowcut %05i \n",param.lowcut = 50 + rand() % 2 * rand() % 951);
+	printf("highcut %05i \n",param.highcut = param.lowcut + 250 + rand() % (10000 - param.lowcut - 250));
+	printf("hum %05i \n",param.hum = rand() % 2 * rand() % 101);
+	printf("noise %05i \n",param.noise = rand() % 2 * rand() % 101);
+	printf("odd %05i \n",param.odd = rand() % 2 * rand() % 101);
+	printf("qsb %05i \n",param.qsb = rand() % 2 * rand() % 101);
+	printf("channels %05i \n",param.channels = 1 + rand() % 2);
+	printf("signals %05i \n",param.signals = 1 + rand() % 5);
+	printf("spacelen %05i \n",param.spacelen = 20 + rand() % 2 * rand() % 281);
+	printf("sweep %05i \n",param.sweep = rand() % 2 * (-4000 + rand() % 8001));
+	printf("sweepness %05i \n",param.sweepness = rand() % 10000);
+	printf("tempo %05i \n",param.tempo = 50 + rand() % 450);
+	printf("window %05i \n",param.window = rand() % 1001);
+	printf("wspaces %05i \n",param.wspaces = rand() % 2 * rand() % 101);
+
+	morsetext = cw_rand_groups(1 + rand() % 50, 0, "abstgjnokqfmzixdrhewlypvcu8219376450?!/=", (((unsigned int) (time(NULL) << 12)) % 32767) + 1);
+
+	printf("\n\n%s\n", morsetext);
+
+	encoded = cw_encode(morsetext);
+
+	cw_initsample(&asound, NULL);
+	cw_initsample(&csound, &asound);
+	csound.bits = 8 + 8 * rand() % 2;
+	cw_signals(&asound, param, encoded);
+	cw_convert(&asound, &csound, 16);
+//	cw_wavout("test.wav", &csound);
+	cw_freesample(&asound);
+	cw_freesample(&csound);
+	cw_free(morsetext);
+	cw_free(encoded);
+}
+
+	return(0);
+}
